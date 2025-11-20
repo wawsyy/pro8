0a818c95b5b33e37eaf1a714a83149973d161542
wswsyy <shiyu689@qq.com>
2025-11-21 00:11:35 +0800

    deploy: deploy EncryptedTemperatureCheck to Sepolia testnet (0x3301AA6508AC4989aF1923dEa7992458bA334B29)

--- frontend/abi/EncryptedTemperatureCheckAddresses.ts
+++ frontend/abi/EncryptedTemperatureCheckAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const EncryptedTemperatureCheckAddresses = { 
-  "11155111": { address: "0xd23109172A4B1B0209985c7e32515023A39d7507", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x3301AA6508AC4989aF1923dEa7992458bA334B29", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
 };
